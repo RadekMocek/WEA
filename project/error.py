d8c30f63eee70bbcf2c663ebe47a4429c2dc1bb7
RadekMocek <radek.mocek@tul.cz>
2023-12-31 05:20:22 +0100

    Zobrazit pouze ne/splněné úkoly

--- project/error.py
+++ project/error.py
@@ -5,5 +5,11 @@ error = Blueprint("error", __name__)
 
 @error.app_errorhandler(404)
 def page_not_found(e):
-    flash("Požadovaná stránka nebyla nalezena.")
+    flash("[404] Požadovaná stránka nebyla nalezena.")
+    return redirect(url_for("main.index"))
+
+
+@error.app_errorhandler(405)
+def method_not_allowed(e):
+    flash("[405] Pro práci s úkoly využijte uživatelského rozhraní.")
     return redirect(url_for("main.index"))
